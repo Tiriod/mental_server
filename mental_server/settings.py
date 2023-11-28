5a3d4a4cd611bd82f76ab14c3f2ac1cf9484489e
时间间 <2964631402@qq.com>
2023-11-28 18:18:05 +0800

    添加了requirements.txt辅助环境安装

--- mental_server/settings.py
+++ mental_server/settings.py
@@ -13,7 +13,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b4w@%w4jft26*(!11$t96zsnrewm73b*uykuwexokg1pcarfsa'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
